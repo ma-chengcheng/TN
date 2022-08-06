7345811bbc22f1d9b0473fde3c5f6ffb9cc4e219
ma-chengcheng <macheng@mail.ustc.edu.cn>
2022-08-06 22:38:50 +0800

    update project

--- TN/cleaner.py
+++ TN/cleaner.py
@@ -1,2 +1,11 @@
-def cleaner(text):
+import re
+
+CHINESE_PATTERN = r'\u4e00-\u9fa5\u3007'
+ENGLISH_PATTERN = r'a-zA-Z'
+NUMBER_PATTERN = r'0-9'
+PUNCTUATION_PATTERN = r'\s\$\(\)\*\+\.\]\[\?\\\^\{\}\|\<\>\°\-\=\,\"\/\‘\’\''
+
+
+def cleaner(text: str) -> str:
+    text = re.sub(f'[^{CHINESE_PATTERN}{ENGLISH_PATTERN}{NUMBER_PATTERN}{PUNCTUATION_PATTERN}]', '', text)
     return text
